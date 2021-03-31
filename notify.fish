65fe9b8444c92330261ce82883a37bee3108d35c
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-31 19:09:53 +0530

    Change: Different message and interval

--- notify.fish
+++ notify.fish
@@ -3,6 +3,6 @@
 while echo "send notify";
 
 paplay ~/Music/notfication/juntos.ogg
-notify-send --urgency=critical Warning! "Move your wrists and Neck\nPosture: Spine straight"
-sleep 30m 
+notify-send --urgency=critical Warning! "Move!" "Spine Straight" "Do a push up" "Do a squat"
+sleep 1h 
 end
