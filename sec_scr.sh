3945b495ddb1d2b326f413ad47fc1c098ee3b75e
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-08-30 17:18:17 +0530

    multi monitor with laptop script

---
+++ sec_scr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# second screen 
+W=1366
+H=768
+# extra output of your server machine.(DP-1, HDMI-0 etc. check with xrandr )
+O=HDMI-0
+if [ "$1" == "create" ]; then
+  gtf $W $H 60 | sed '3q;d' | sed 's/Modeline//g' | xargs xrandr --newmode
+  # sed: get third line, delete 'Modeline', get first word, remove first and last characters
+  gtf $W $H 60 | sed '3q;d' | sed 's/Modeline//g' | awk '{print $1;}' | sed 's/^.\(.*\).$/\1/' | xargs xrandr --addmode $O
+  gtf $W $H 60 | sed '3q;d' | sed 's/Modeline//g' | awk '{print $1;}' | sed 's/^.\(.*\).$/\1/' | xargs xrandr --output $O --right-of DP-2 --mode
+elif [ "$1" == "on" ]; then
+  x11vnc -clip ${W}x${H}+1920+0
+else
+  echo "missing argument: [create | on]"
+fi
+
