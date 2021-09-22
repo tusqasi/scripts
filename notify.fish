86efe3876a34c3a456f4245fcc8e7921fb02dc5f
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-09-22 21:41:44 +0530

    pruned

--- notify.fish
+++
@@ -1,8 +0,0 @@
-#!/bin/fish
-
-while echo "send notify";
-
-paplay ~/Music/notfication/juntos.ogg
-notify-send --urgency=critical Warning! "Move!" "Spine Straight" "Do a push up" "Do a squat"
-sleep 1h 
-end
