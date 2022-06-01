ba819fcb03670c62538ca8a7a0a84ec1c9f889db
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-06-01 20:21:41 +0530

    ♻ More comments

--- rofi_tmux.py
+++ rofi_tmux.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+
 import subprocess
 import utils
 
 terminal = "alacritty"
 
 
-def get_sessions():
+def get_sessions() -> list[str]:
+    """Get open tmux session"""
     sessions: list[str] = utils.run_shell_cmd(
         ["tmux list-session", "-F '#S'"],
     ).split()
     return sessions
 
 
-def get_choice(sessions):
-    """get session choice"""
+def get_choice(sessions: str) -> str:
+    """Get session choice"""
     sessions: bytes = "\n".join(sessions).encode()
     cmd: list[str] = ["rofi", "-dmenu", '-p "Select existing tmux session"']
 
@@ -35,6 +37,8 @@ def main():
     choice = get_choice(sessions)
     if len(choice) == 0:
         return 0
+
+    """New session with name given in the input"""
     subprocess.Popen(
         [f"{terminal} -e tmux new-session -A -s {choice}"],
         shell=True,
@@ -46,5 +50,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # testing()
     main()
